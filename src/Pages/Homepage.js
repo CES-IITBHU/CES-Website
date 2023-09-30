ed22fd716c4d4175a27fd12100838770d11924d1
AbhijeetMankani <abhijeetmankani@gmail.com>
2023-10-01 00:49:48 +0530

    Homepage modified

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -5,6 +5,7 @@ import BG from "../Images/image.png";
 import Footer from "../Components/Footer";
 import ImageGallery from "../Components/ImageGallery";
 // import video from '../Video/Civil_Dept_vid.mp4';
+import "./Homepage.css"
 
 const Homepage = () => {
   return (
@@ -16,14 +17,13 @@ const Homepage = () => {
           style={{ backgroundImage: `url(${BG})` }}
         >
           <div className="md:w-[40%] w-[70%] relative md:top-40 top-20 ml-10 md:ml-16">
-            <div className="text-3xl  text-white font-bold">
-              The Society of Civil Engineers
+            <div className="text-header">
+              <h>The Society of Civil Engineers</h>
             </div>
-            <div className="text-white mt-7 text-xl">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-              odio officia possimus qui temporibus. Culpa magnam maxime iure at,
-              ut non numquam laboriosam cum optio labore mollitia doloremque
-              consequatur cumque.
+            <div className="text">
+            <p>Civil Engineering Society is the interaction community of the civil engineers, often referred to as CES,  It serves as a link between enrolled civil engineering students, alumni and professors at IIT BHU
+            The Civil Engineering Society typically refers to a community  dedicated to the field of civil engineering. The CES is an essential hub for students of civil engineering , offering resources, networking opportunities, and career enhancement.</p>
+
             </div>
           </div>
           <img
@@ -36,31 +36,27 @@ const Homepage = () => {
       </div>
       <div className="mt-10 mb-20">
         <div className="text-4xl text-center font-bold">About Us</div>
-        <div className="md:mx-48 mx-5 mt-10">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-          esse labore ut tempora optio soluta hic? Sed exercitationem explicabo
-          quod natus repellendus voluptate doloribus, assumenda ex id! Nemo
-          consequatur quibusdam numquam alias ut unde nesciunt placeat est
-          incidunt ab, voluptates quis, aperiam expedita rem optio quod sapiente
-          reprehenderit fugit odit neque. Officia, quisquam! Doloribus fugiat
-          sint autem voluptates architecto molestiae ea minima consequatur et
-          pariatur quam maxime dolorum eligendi culpa ut sequi iste laborum,
-          expedita labore praesentium magnam quibusdam, laboriosam quis aperiam.
-          Sequi voluptatem maxime incidunt quidem deserunt assumenda sunt
-          laborum et nam officia atque, fugiat harum deleniti placeat natus!
+        <div className="container">
+        This society is an effort to allow communication and sharing of ideas and information among all upcoming civil engineers and aim to provide a platform to show off their skills in every part of their life. The Society plays an active role in improving the Faculty-Student interaction in the department which involves Events, Talk Shows, Workshops, Hackathons and many more…
+CES also conducts SHILP - one of the biggest cultural event of the civil engineering society it is more than just a fest , it's a fest where students explore their creative as well as technical aspects of civil engineering.Undoubtedly! Team is the backbone of any  society in CES, you'll encounter a diverse team alligned towards same mission and purpose.
+
         </div>
-        <div className="md:mx-48 mx-5 mt-5">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-          esse labore ut tempora optio soluta hic? Sed exercitationem explicabo
-          quod natus repellendus voluptate doloribus, assumenda ex id! Nemo
-          consequatur quibusdam numquam alias ut unde nesciunt placeat est
-          incidunt ab, voluptates quis, aperiam expedita rem optio quod sapiente
-          reprehenderit fugit odit neque. Officia, quisquam! Doloribus fugiat
-          sint autem voluptates architecto molestiae ea minima consequatur et
-          pariatur quam maxime dolorum eligendi culpa ut sequi iste laborum,
-          expedita labore praesentium magnam quibusdam, laboriosam quis aperiam.
-          Sequi voluptatem maxime incidunt quidem deserunt assumenda sunt
-          laborum et nam officia atque, fugiat harum deleniti placeat natus!
+        <div className="container">
+          <h>Alumni Interaction</h>
+          <p> Alumni Interaction Team dedicates themselves to strengthen the bond between alumni and students. They facilitate mentorship for students to seek guidance and learn from alumni experiences.</p>
+          <br></br>
+
+          <h>Public Relations</h>
+          <p>  Public Relations Team acts as the welcoming face of CES. They foster connections with industry leaders and organizations, guaranteeing that our message reaches across vast horizons.</p>
+          <br></br>
+          <h>Events</h>
+          <p> The magic-makers Events Team turns ideas into unforgettable experiences by organizing workshops and insightful seminars</p>
+          <br></br>
+          <h>Creative Team</h>
+          <p> Team of creative peeps responsible for brainstorming and generating attention-grabbing ideas and concepts.
+</p>
+          <br></br>
+          <p>Together, we all are the driving force that ensures effective working of our society , it's this unity that allows us to overcome challenges, push boundaries, and leave a lasting legacy of innovation and progress.</p>
         </div>
       </div>
       {/* <video controls className="w-[60%] block m-auto mb-3">
