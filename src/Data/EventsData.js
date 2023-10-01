dab00f08e11fb8ba258bf98311d43e98ea23f539
AbhijeetMankani <abhijeetmankani@gmail.com>
2023-10-01 23:11:25 +0530

    Added All Latest Events

--- src/Data/EventsData.js
+++ src/Data/EventsData.js
@@ -11,9 +11,216 @@ import Event10 from '../Images/event10.JPG'
 import Event11 from '../Images/event11.JPG'
 import Event12 from '../Images/event12.jpeg'
 import Event13 from '../Images/event13.jpeg'
-
+import Event14 from '../Images/Events/2nd Year Orientation/IMG_6255.jpg'
+import Event15 from '../Images/Events/2nd Year Orientation/IMG_6256.jpg'
+import Event16 from '../Images/Events/Chandrayaan Landing/IMG_6322.jpg'
+import Event17 from '../Images/Events/Chandrayaan Landing/IMG_6323.jpg'
+import Event18 from '../Images/Events/Chandrayaan Landing/IMG_6327.jpg'
+import Event19 from '../Images/Events/Chandrayaan Landing/IMG_6328.jpg'
+import Event20 from '../Images/Events/Chandrayaan Landing/IMG_6329.jpg'
+import Event21 from '../Images/Events/Teacher_s Day 2023 Celebrations/IMG_6352.jpg'
+import Event22 from '../Images/Events/Teacher_s Day 2023 Celebrations/IMG_6354.jpg'
+import Event23 from '../Images/Events/Teacher_s Day 2023 Celebrations/IMG_6355.jpg'
+import Event24 from '../Images/Events/Teacher_s Day 2023 Celebrations/IMG_6359.jpg'
+import Event25 from '../Images/Events/Teacher_s Day 2023 Celebrations/IMG_6360.jpg'
+import Event26 from '../Images/Events/Teacher_s Day 2023 Celebrations/IMG_6361.jpg'
+import Event27 from '../Images/Events/Teacher_s Day 2023 Celebrations/IMG_6362.jpg'
+import Event28 from '../Images/Events/Teacher_s Day 2023 Celebrations/IMG_6363.jpg'
+import Event29 from '../Images/Events/Teacher_s Day 2023 Celebrations/IMG_6364.jpg'
 
 const Data = [
+  {
+    id: 21,
+    image: Event21,
+    topic: "Teacher Day 2023",
+    content:
+      "",
+    location: "",
+    time: "",
+    date: "5/9/23",
+    place: "flex-row",
+    color: "bg-red-400",
+  },
+  {
+    id: 22,
+    image: Event22,
+    topic: "Teacher Day 2023",
+    content:
+      "",
+    location: "",
+    time: "",
+    date: "5/9/23",
+    place: "flex-row",
+    color: "bg-red-400",
+  },
+  {
+    id: 23,
+    image: Event23,
+    topic: "Teacher Day 2023",
+    content:
+      "",
+    location: "",
+    time: "",
+    date: "5/9/23",
+    place: "flex-row",
+    color: "bg-red-400",
+  },
+  {
+    id: 24,
+    image: Event24,
+    topic: "Teacher Day 2023",
+    content:
+      "",
+    location: "",
+    time: "",
+    date: "5/9/23",
+    place: "flex-row",
+    color: "bg-red-400",
+  },
+  {
+    id: 25,
+    image: Event25,
+    topic: "Teacher Day 2023",
+    content:
+      "",
+    location: "",
+    time: "",
+    date: "5/9/23",
+    place: "flex-row",
+    color: "bg-red-400",
+  },
+  {
+    id: 26,
+    image: Event26,
+    topic: "Teacher Day 2023",
+    content:
+      "",
+    location: "",
+    time: "",
+    date: "5/9/23",
+    place: "flex-row",
+    color: "bg-red-400",
+  },
+  {
+    id: 27,
+    image: Event27,
+    topic: "Teacher Day 2023",
+    content:
+      "",
+    location: "",
+    time: "",
+    date: "5/9/23",
+    place: "flex-row",
+    color: "bg-red-400",
+  },
+  {
+    id: 28,
+    image: Event28,
+    topic: "Teacher Day 2023",
+    content:
+      "",
+    location: "",
+    time: "",
+    date: "5/9/23",
+    place: "flex-row",
+    color: "bg-red-400",
+  },
+  {
+    id: 29,
+    image: Event29,
+    topic: "Teacher Day 2023",
+    content:
+      "",
+    location: "",
+    time: "",
+    date: "5/9/23",
+    place: "flex-row",
+    color: "bg-red-400",
+  },
+  {
+    id: 16,
+    image: Event16,
+    topic: "Chandrayan Landing",
+    content:
+      "",
+    location: "",
+    time: "",
+    date: "23/8/23",
+    place: "flex-row",
+    color: "bg-red-400",
+  },
+  {
+    id: 17,
+    image: Event17,
+    topic: "Chandrayan Landing",
+    content:
+      "",
+    location: "",
+    time: "",
+    date: "23/8/23",
+    place: "flex-row",
+    color: "bg-red-400",
+  },
+  {
+    id: 18,
+    image: Event18,
+    topic: "Chandrayan Landing",
+    content:
+      "",
+    location: "",
+    time: "",
+    date: "23/8/23",
+    place: "flex-row",
+    color: "bg-red-400",
+  },
+  {
+    id: 19,
+    image: Event19,
+    topic: "Chandrayan Landing",
+    content:
+      "",
+    location: "",
+    time: "",
+    date: "23/8/23",
+    place: "flex-row",
+    color: "bg-red-400",
+  },
+  {
+    id: 20,
+    image: Event20,
+    topic: "Chandrayan Landing",
+    content:
+      "",
+    location: "",
+    time: "",
+    date: "23/8/23",
+    place: "flex-row",
+    color: "bg-red-400",
+  },
+  {
+    id: 14,
+    image: Event14,
+    topic: "2nd Year Orientation",
+    content:
+      "",
+    location: "",
+    time: "",
+    date: "29/7/23",
+    place: "flex-row",
+    color: "bg-red-400",
+  },
+  {
+    id: 15,
+    image: Event15,
+    topic: "2nd Year Orientation",
+    content:
+      "",
+    location: "",
+    time: "",
+    date: "29/7/23",
+    place: "flex-row",
+    color: "bg-red-400",
+  },
   {
     id: 1,
     image: Event1,
@@ -159,18 +366,19 @@ const Data = [
     place: "flex-row",
     color: "bg-red-400",
   },
-   {
-    id: 13,
-    image: Event13,
-    topic: "Intra civil Cricket Tournament",
-    content:
-      "",
-    location: "ADV Ground",
-    time: "",
-    date: "4/02/23",
-    place: "flex-row",
-    color: "bg-red-400",
-  },
+  {
+   id: 13,
+   image: Event13,
+   topic: "Intra civil Cricket Tournament",
+   content:
+     "",
+   location: "ADV Ground",
+   time: "",
+   date: "4/02/23",
+   place: "flex-row",
+   color: "bg-red-400",
+ },
+
 
 ];
 
